31b07e03812497fd88a965b36be06bc1f202289d
__tanglymeng <111646589+TangLymeng@users.noreply.github.com>
2024-04-09 14:00:05 +0700

    add some template

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Homepage from "./components/Homepage/Homepage"
+import HomePostsLists from "./Templates/HomePostListsTemplate"
+import LoginTemplate from "./Templates/LoginTemplate"
+import NavbarTemplate from "./Templates/NavBarTemplate"
+import UserProfileTemplate from "./Templates/UserProfileTemplate"
 
 export default function App() {
   
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Homepage />}></Route>
+        <Route path="/login" element={<UserProfileTemplate />}></Route>
       </Routes>
     </BrowserRouter>
   )
