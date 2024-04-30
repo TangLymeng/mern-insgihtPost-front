5611f1630b0ea14096e0e543ce47f1928a17b44f
__tanglymeng <111646589+TangLymeng@users.noreply.github.com>
2024-04-30 14:41:25 +0700

    make some change about comment functionallity

--- src/components/Comments/AddComment.js
+++ src/components/Comments/AddComment.js
@@ -28,46 +28,13 @@ const AddComment = ({ postId, comments }) => {
     dispatch(createCommentAction({ ...formData, postId }));
   };
 
-  <section class="bg-white dark:bg-gray-900 py-8 lg:py-16 antialiased">
-    <div class="max-w-2xl mx-auto px-4">
-      <div class="flex justify-between items-center mb-6">
-        <h2 class="text-lg lg:text-2xl font-bold text-gray-900 dark:text-white">
-          Discussion (20)
-        </h2>
-      </div>
-      <form class="mb-6" onSubmit={handleSubmit}>
-        <div class="py-2 px-4 mb-4 bg-white rounded-lg rounded-t-lg border border-gray-200 dark:bg-gray-800 dark:border-gray-700">
-          <label for="comment" class="sr-only">
-            Your comment
-          </label>
-          <textarea
-            id="comment"
-            rows="6"
-            class="px-0 w-full text-sm text-gray-900 border-0 focus:ring-0 focus:outline-none dark:text-white dark:placeholder-gray-400 dark:bg-gray-800"
-            placeholder="Write a comment..."
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            required
-          ></textarea>
-        </div>
-        <button
-          type="submit"
-          class="inline-flex items-center py-2.5 px-4 text-xs font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
-        >
-          Post comment
-        </button>
-      </form>
-    </div>
-  </section>;
-
   return (
     
     <section class="bg-white py-8 lg:py-16 antialiased">
       <div class="max-w-7xl mx-auto px-4">
         <div class="flex justify-between items-center mb-6">
           <h2 class="text-lg lg:text-2xl font-bold text-gray-900">
-            Discussion ({comments.length})
+            Discussion ({comments?.length})
           </h2>
         </div>
         <form class="mb-6" onSubmit={handleSubmit}>
